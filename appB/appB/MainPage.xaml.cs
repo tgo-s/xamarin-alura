fba5d6a92f9ee14abcddd8829c05fb675f58c7f0
Thiago Simionato dos Santos <thiago.santos@sanepar.com.br>
2018-09-20 08:24:57 -0300

    primeira aula do alura finalizada - appB

---
+++ appB/appB/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace appB
+{
+    public partial class MainPage : ContentPage
+    {
+        public List<Veiculo> Veiculos { get; set; }
+        public MainPage()
+        {
+            //Fazendo analogia com MVC, no BidingContext vc define sua classe de View
+            this.BindingContext = new VeiculoView();
+
+            InitializeComponent();
+        }
+    }
+
+    public class VeiculoView
+    {
+        public List<Veiculo> Veiculos { get; set; }
+        public VeiculoView()
+        {
+            this.Veiculos = new List<Veiculo>()
+            {
+                new Veiculo { Nome="Camaro", Preco=170000},
+                new Veiculo {Nome="Golf", Preco=75000},
+                new Veiculo {Nome="Lancer EVO X", Preco=120000}
+            };
+        }
+    }
+
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
