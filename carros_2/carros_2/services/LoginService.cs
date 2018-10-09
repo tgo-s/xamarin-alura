bf1a7a1b67604f13f7c190c5c762b7b077eac2f5
Thiago Simionato dos Santos <thiago.santos@sanepar.com.br>
2018-10-09 16:07:11 -0300

    Fim da implementação da parte 3 do curso de xamarin alura

--- carros_2/carros_2/services/LoginService.cs
+++ carros_2/carros_2/services/LoginService.cs
@@ -1,7 +1,10 @@
 ﻿using carros_2.models;
+using carros_2.util;
 using Javax.Security.Auth.Login;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using Xamarin.Forms;
@@ -12,7 +15,9 @@ namespace carros_2.services
     {
         public async System.Threading.Tasks.Task FazerLogin(Login login)
         {
-            using (HttpClient client = new HttpClient())
+            HttpClientHandler httpClientHandler = new ProxyConfig().CreateSaneparProxyConfig();
+
+            using (HttpClient client = new HttpClient(handler: httpClientHandler, disposeHandler: true))
             {
                 FormUrlEncodedContent camposForm = new FormUrlEncodedContent(new[]
                 {
@@ -24,9 +29,15 @@ namespace carros_2.services
                 try
                 {
                     //client.BaseAddress = new System.Uri("http://127.0.0.1:5000/api");
-                     var result = await client.PostAsync("http://127.0.0.1:5000/api/login/auth/", camposForm);
+                    //var result = await client.PostAsync("http://127.0.0.1:5000/api/login/auth/", camposForm);
+                    
+                    var result = await client.PostAsync("http://aluracar.herokuapp.com/login", camposForm);
                     if (result.IsSuccessStatusCode)
-                        MessagingCenter.Send<Usuario>(new Usuario(), "SucessoLogin");
+                    {
+                        var conteudo = await result.Content.ReadAsStringAsync();
+                        var jsonObject = JsonConvert.DeserializeObject<ResultadoLogin>(conteudo);
+                        MessagingCenter.Send<Usuario>(jsonObject.Usuario, "SucessoLogin");
+                    }
                     else
                         MessagingCenter.Send<LoginException>(new LoginException("Usuario ou senha inválidos"), "FalhaLogin");
                 }
--- carros_2/carros_2/viewmodel/MasterViewModel.cs
+++ carros_2/carros_2/viewmodel/MasterViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 using carros_2.models;
+using Xamarin.Forms;
 
 namespace carros_2.viewmodel
 {
-    public class MasterViewModel
+    public class MasterViewModel : ViewModelBase
     {
         public string Nome
         {
@@ -18,6 +20,21 @@ namespace carros_2.viewmodel
         public MasterViewModel(Usuario usuario)
         {
             this.usuario = usuario;
+            DefinirComandos(usuario);
+        }
+
+        private void DefinirComandos(Usuario usuario)
+        {
+            EditarPerfilCommand = new Command(() =>
+            {
+                MessagingCenter.Send<Usuario>(usuario, "EditarPerfil");
+                
+            });
+
+            SalvarPerfilCommand = new Command(() =>
+            {
+                MessagingCenter.Send<Usuario>(usuario, "SalvarPerfil");
+            });
         }
 
         public string Email
@@ -26,6 +43,31 @@ namespace carros_2.viewmodel
             set { this.usuario.Email = value; }
         }
 
+        public string DataNascimento
+        {
+            get { return usuario.DataNascimento; }
+            set { usuario.DataNascimento = value; }
+        }
+
+        public string Telefone
+        {
+            get { return usuario.Telefone; }
+            set { usuario.Telefone = value; }
+        }
+
+        private bool editando = false;
+        public bool Editando
+        {
+            get { return editando; }
+            set
+            {
+                editando = value;
+                OnPropertyChanged(nameof(Editando));
+            }
+        }
+
+        public ICommand EditarPerfilCommand { get; private set; }
+        public ICommand SalvarPerfilCommand { get; private set; }
 
     }
 }
